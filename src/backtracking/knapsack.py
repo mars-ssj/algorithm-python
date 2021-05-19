6d820a26e90dc2a90fb0ef1325c8022b98484ec0
shenshaojie <shenshaojie@kuaishou.com>
2021-05-19 21:06:51 +0800

    test knapsack

--- src/backtracking/knapsack.py
+++ src/backtracking/knapsack.py
@@ -5,10 +5,10 @@ class Knapsack(object):
         super().__init__()
         self.max_weight = -1
     
-    def solution(self, weights, w):
-        self.solution_action(0, 0, w, weights, len(w))
+    def solution(self, weights, weight_limit):
+        self.action(0, 0, weight_limit, weights, len(weights))
 
-    def solution_action(self, cn, cw, w, weights, n):
+    def action(self, cn, cw, w, weights, n):
         '''
             w: 背包容纳重量
             cw: 当前重量
@@ -17,9 +17,27 @@ class Knapsack(object):
             cn: 当前遍历到第几个
         '''
         if cn == n or cw == w:
-            if w > self.max_weight:
-                self.max_weight = w
+            if cw > self.max_weight:
+                self.max_weight = cw
             return
-        self.dp(cn+1, cw, w, weights, n)
+        self.action(cn+1, cw, w, weights, n)
         if cw + weights[cn] <= w:
-            self.dp(cn+1, cw+weights[cn], w, weights, n)
+            self.action(cn+1, cw+weights[cn], w, weights, n)
+
+class KnapsackMaxValue(object):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.max_value = -1
+    
+    def solution(self, weights, weight_limit, values):
+        self.action(0, 0, weight_limit, weights, 0, values, len(weights))
+
+    def action(self, cn, cw, w, weights, cv, values, n):
+        if cw == w or cn == n:
+            if cv > self.max_value:
+                self.max_value = cv
+            return
+        self.action(cn+1, cw, w, weights, cv, values, n)
+        if cw + weights[cn] <= w:
+            self.action(cn+1, cw+weights[cn], w, weights, cv+values[cn], values, n)
--- src/backtracking/knapsack_test.py
+++ src/backtracking/knapsack_test.py
@@ -5,14 +5,22 @@ import unittest
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 from backtracking.knapsack import Knapsack
+from backtracking.knapsack import KnapsackMaxValue
 
 class TestPattern(unittest.TestCase):
 
-    def test_pattern(self):
+    def test_max_weight(self):
         p = Knapsack()
-        weights = [2, 2, 4, 5, 6, 3]
-        p.dp(0, 0, 9, weights, 6)
+        weights = [2, 2, 4, 6, 3]
+        p.solution(weights, 9)
         self.assertEqual(p.max_weight, 9)
+    
+    def test_max_value(self):
+        k = KnapsackMaxValue()
+        weights = [2, 2, 4, 6, 3]
+        values = [3, 4, 8, 9, 6]
+        k.solution(weights, 9, values)
+        self.assertEqual(k.max_value, 18)
 
 if __name__ == '__main__':
     unittest.main()
